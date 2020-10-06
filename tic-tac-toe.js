c7701e108838726d0d0ffe39d26573dcdd597215
sadlerb <sadlerb22@gmail.com>
2020-10-06 00:02:46 -0500

    Finished excercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,32 @@
+var score = [];
 document.addEventListener('DOMContentLoaded', () => {
     var tiles = document.getElementById("board").children;
     for (let i = 0; i < tiles.length; i++) {
         const element = tiles[i];
         element.classList.add('square');
+        element.addEventListener("click", function() {
+            fillSquare(element);
+        });
         
     }
 });
+
+
+function fillSquare(element) {
+    element.className += " X";
+    element.innerHTML = "X"
+    scoreKeeper();
+       
+}
+
+
+function scoreKeeper(){
+    
+}
+
+
+
+
+
 
 
