e7c842283f6fd992163156dbfc88846361def1e9
sadlerb <sadlerb22@gmail.com>
2020-10-06 00:28:34 -0500

    Finished excercise 3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,4 @@
-var score = [];
+var turn = [];
 document.addEventListener('DOMContentLoaded', () => {
     var tiles = document.getElementById("board").children;
     for (let i = 0; i < tiles.length; i++) {
@@ -7,21 +7,43 @@ document.addEventListener('DOMContentLoaded', () => {
         element.addEventListener("click", function() {
             fillSquare(element);
         });
+        element.addEventListener("mouseover", function(){
+            mouseOver(element);
+        });
+        element.addEventListener("mouseout", function(){
+            mouseOut(element);
+        });
         
     }
 });
 
 
 function fillSquare(element) {
-    element.className += " X";
-    element.innerHTML = "X"
-    scoreKeeper();
+    if (turn.length == 0 || turn[turn.length-1] == "O"){
+        element.className += " X";
+        element.innerHTML = "X";
+        turnKeeper("X")
+    }else{
+        console.log(turn[length-1])
+        element.className += " O";
+        element.innerHTML = "O";
+        turnKeeper("O");
+    }
+    
        
 }
 
+function mouseOver(element){
+    element.classList.add('hover');
+}
 
-function scoreKeeper(){
-    
+function mouseOut(element){
+    element.classList.remove('hover');  
+}
+
+
+function turnKeeper(player){
+    turn.push(player);
 }
 
 
